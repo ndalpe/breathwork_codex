65b650b0e5c9f78bce2c27661f224d6351ded4a2
nicolas-dalpe <55551039+nicolas-dalpe@users.noreply.github.com>
2026-06-02 20:51:38 -0400

    app: Initial build.

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,15 +1,26 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(fetch(event.request));
+
+  event.respondWith(
+    fetch(event.request, {
+      cache: "no-store",
+      credentials: event.request.credentials,
+      redirect: event.request.redirect
+    }).catch(() => fetch(event.request))
+  );
 });
